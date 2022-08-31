6a2b7a1ed1e2083a4519fb7991372269f821d80f
mho3eind <m.hosein.987@gmail.com>
2022-08-31 11:08:46 +0430

    change dashboard controller

--- controller/userDashboardController.js
+++ controller/userDashboardController.js
@@ -1,28 +1,25 @@
-const fs = require("fs");
+const fs = require('fs')
 
-const multer = require("multer");
-const shortId = require("shortid");
-const sharp = require("sharp");
-const appRoot = require("app-root-path");
+const multer = require('multer')
+const shortId = require('shortid')
+const sharp = require('sharp')
+const appRoot = require('app-root-path')
 
-const { formatDate } = require("../utils/jalali");
-const { get500, get404 } = require("./errorController");
-const { fileFilter } = require("../utils/multer");
+const { formatDate } = require('../utils/jalali')
+const { get500, get404 } = require('./errorController')
+const { fileFilter } = require('../utils/multer')
 
 exports.getDashboard = async (req, res) => {
   try {
-    res.set(
-      "Cache-Control",
-      "no-cache, private, no-store, must-revalidate, max-stale=0, post-check=0, pre-check=0"
-    );
+    res.set('Cache-Control', 'no-cache, private, no-store, must-revalidate, max-stale=0, post-check=0, pre-check=0')
 
-    res.render("newDashboard", {
-      pageTitle: "مدیریت حساب کاربری",
-      path: "/dashboard",
+    res.render('dashboard', {
+      pageTitle: 'مدیریت حساب کاربری',
+      path: '/dashboard',
       fullname: req.user.fullname,
-    });
+    })
   } catch (err) {
-    console.log(err);
-    get500(req, res);
+    console.log(err)
+    get500(req, res)
   }
-};
+}
