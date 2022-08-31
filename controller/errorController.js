81581ec03f398d2626dd71e712539798e811e47c
mho3eind <m.hosein.987@gmail.com>
2022-08-31 23:06:22 +0430

    make 500 page

--- controller/errorController.js
+++ controller/errorController.js
@@ -1,13 +1,13 @@
 exports.get404 = (req, res) => {
-  res.render("404", {
-    pageTitle: "صفحه پیدا نشد | 404",
-    path: "/404",
-  });
-};
+  res.render('404', {
+    pageTitle: 'صفحه پیدا نشد | 404',
+    path: '/404',
+  })
+}
 
 exports.get500 = (req, res) => {
-  res.render("500", {
-    pageTitle: "صفحه پیدا نشد | 404",
-    path: "/500",
-  });
-};
+  res.render('500', {
+    pageTitle: 'صفحه پیدا نشد | 404',
+    path: '/500',
+  })
+}
