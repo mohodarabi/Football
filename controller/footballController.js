020334c20229c5a18dda30abcbc199247aee7442
mho3eind <m.hosein.987@gmail.com>
2022-09-04 22:41:09 +0430

    change last match view

--- controller/footballController.js
+++ controller/footballController.js
@@ -2,11 +2,14 @@ const Match = require('../model/Match')
 
 exports.getIndex = async (req, res) => {
   try {
-    const matches = await Match.find({ $and: [{ startForecast: { $lt: Date.now() } }, { endForecast: { $gte: Date.now() } }, { isFinished: false }] })
+    const matches = await Match.find({ $and: [{ startForecast: { $lt: Date.now() } }, { endForecast: { $gte: Date.now() } }, { isFinished: false }] }).sort()
+
+    const match = matches[0]
 
     res.render('index', {
       pageTitle: 'صفحه ی اصلی',
       matches,
+      match,
       path: '/',
     })
   } catch (err) {
