72ac778bb014d5f2ad88e7667e85f4860cbc0370
mho3eind <m.hosein.987@gmail.com>
2022-09-04 21:52:06 +0430

    handle forecast in index page

--- controller/footballController.js
+++ controller/footballController.js
@@ -1,7 +1,12 @@
+const Match = require('../model/Match')
+
 exports.getIndex = async (req, res) => {
   try {
+    const matches = await Match.find({ $and: [{ startForecast: { $lt: Date.now() } }, { endForecast: { $gte: Date.now() } }, { isFinished: false }] })
+
     res.render('index', {
       pageTitle: 'صفحه ی اصلی',
+      matches,
       path: '/',
     })
   } catch (err) {
